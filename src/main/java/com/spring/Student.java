2a350e9d6ab0e50f62e5f6cf0de7a9f3e81102f7
anita <anubhui27@gmail.com>
2023-02-07 10:56:43 +0530

    add spring project

---
+++ src/main/java/com/spring/Student.java
@@ -0,0 +1,16 @@
+package com.spring;
+
+public class Student {
+	private String city;
+
+	public void setCity(String city) {
+		this.city = city;
+
+	}
+
+	public void getmessage(String name) {
+		System.out.println("Hi,"+name+","+city);
+	}
+	
+
+}
